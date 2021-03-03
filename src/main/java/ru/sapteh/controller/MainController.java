a1164b0f1e855a838bb1dec28ccf7748339f9e93
Danil Stozhkov <lekseih2000>
2021-03-03 13:40:51 +0300

    PdF

--- src/main/java/ru/sapteh/controller/MainController.java
+++ src/main/java/ru/sapteh/controller/MainController.java
@@ -1,5 +1,10 @@
 package ru.sapteh.controller;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,6 +17,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -22,7 +28,9 @@ import ru.sapteh.model.Gender;
 import ru.sapteh.service.ClientDaoImpl;
 import ru.sapteh.service.GenderService;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -136,6 +144,67 @@ public class MainController {
         stage.setTitle("Регистрация пользователя");
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @FXML
+    public void pressSavePDF(ActionEvent event) throws IOException, DocumentException {
+        FileChooser fileChooser=new FileChooser();
+        fileChooser.setTitle("Выберите путь");
+        File file=fileChooser.showSaveDialog(new Stage());
+        String fileName= file.getAbsolutePath();
+        Document document=new Document();
+        PdfWriter.getInstance(document,new FileOutputStream(fileName));
+        document.open();
+
+        Image image=Image.getInstance("C:\\JavaProgect\\AutoservicePrim2\\src\\main\\resources\\image\\logo.png");
+        image.scaleAbsoluteHeight(20);
+        image.scaleAbsoluteWidth(130);
+        image.setAlignment(Element.ALIGN_RIGHT);
+        document.add(image);
+        String font = "./src/main/resources/font/ArialRegular.ttf";
+
+        BaseFont bf = BaseFont.createFont(font, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        Font fontParagraph = new Font(bf,30,Font.NORMAL);
+        Font fontTable=new Font(bf,12,Font.NORMAL);
+
+        Paragraph paragraph=new Paragraph("Clients Автосервиса: ",fontParagraph);
+        paragraph.setSpacingAfter(20);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(paragraph);
+
+        int numColumns=tableClient.getColumns().size();
+
+        PdfPTable tablePdf=new PdfPTable(numColumns);
+        ObservableList<TableColumn<Client, ?>> columns = tableClient.getColumns();
+        columns.forEach(c->tablePdf.addCell(new PdfPCell(new Phrase(c.getText(),fontTable))));
+        tablePdf.setHeaderRows(1);
+        for (int i = 0; i < tableClient.getItems().size(); i++) {
+            tablePdf.addCell(new PdfPCell(new Phrase(String.valueOf(tableClient.getItems().get(i).getId()),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(String.valueOf(tableClient.getItems().get(i).getGender().getCode()),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getLastName(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getFirstName(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getPatronymic(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getBirthday().toString(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getPhone(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getEmail(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getRegistrationDate().toString(),fontTable)));
+            Set<ClientService> services = tableClient.getItems().get(i).getServices();
+            String lastDate="";
+            if (services.size()!=0){
+                Date date=services.stream().max(Comparator.comparing(ClientService::getStartTime)).get().getStartTime();
+               lastDate=new SimpleDateFormat("dd.MM.yyyy").format(date);
+            }
+            tablePdf.addCell(new PdfPCell(new Phrase(lastDate,fontTable)));
+            //tablePdf.addCell(new PdfPCell(new Phrase(tableClient.getItems().get(i).getServices().stream().max(Comparator.comparing(ClientService::getStartTime)).get().getStartTime().toString(),fontTable)));
+            tablePdf.addCell(new PdfPCell(new Phrase(String.valueOf(tableClient.getItems().get(i).getServices().size()),fontTable)));
+
+        }
+        document.add(tablePdf);
+        document.close();
+
+
+
+
     }
     private void pageTableView(){
         sizeClients=clients.size();
@@ -256,6 +325,8 @@ public class MainController {
             UpdateClientController.client.setEmail(email);
             UpdateClientController.client.setGender(client.getGender());
             UpdateClientController.client.setBirthday(client.getBirthday());
+            UpdateClientController.client.setRegistrationDate(client.getRegistrationDate());
+            UpdateClientController.client.setPhotoPath(client.getPhotoPath());
         }else
             idClient=0;
          firstName="";
--- src/main/java/ru/sapteh/controller/UpdateClientController.java
+++ src/main/java/ru/sapteh/controller/UpdateClientController.java
@@ -1,16 +1,28 @@
 package ru.sapteh.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import ru.sapteh.model.Client;
+import ru.sapteh.model.Gender;
+import ru.sapteh.service.ClientDaoImpl;
+import ru.sapteh.service.GenderService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class UpdateClientController {
+
+    ObservableList<Gender> genders= FXCollections.observableArrayList();
     @FXML
     private TextField txtId;
 
@@ -34,20 +46,22 @@ public class UpdateClientController {
 
     @FXML
     private Button buttonToClose;
-
     @FXML
-    private TextField txtGender;
+    private ComboBox<Gender> comboGender;
+
     public static Client client=new Client();
     @FXML
     public void initialize(){
-        txtId.setText(String.valueOf(MainController.idClient));
         txtFirstName.setText(client.getFirstName());
         txtEmail.setText(client.getEmail());
-        txtGender.setText(String.valueOf(client.getGender().getCode()));
         txtLastName.setText(client.getLastName());
         txtPhone.setText(client.getPhone());
         txtPatronymic.setText(client.getPatronymic());
-        pickerBirthday.getEditor().setText(String.valueOf(client.getBirthday()));
+        pickerBirthday.getEditor().setText(client.getBirthday().toString());
+        getGender();
+        comboGender.setItems(genders);
+        comboGender.setValue(genders.get(0));
+
     }
 
     @FXML
@@ -56,7 +70,24 @@ public class UpdateClientController {
     }
 
     @FXML
-    void updateClient(ActionEvent event) {
+    void updateClient(ActionEvent event) throws ParseException {
+        SessionFactory factory=new Configuration().configure().buildSessionFactory();
+        ClientDaoImpl clientDao=new ClientDaoImpl(factory);
+        client.setFirstName(txtFirstName.getText());
+        client.setLastName(txtLastName.getText());
+        client.setPhone(txtPhone.getText());
+        client.setEmail(txtEmail.getText());
+        client.setBirthday(new SimpleDateFormat("yyyy-MM-dd").parse(String.valueOf(pickerBirthday.getValue())));
+        client.setGender(comboGender.getValue());
+        client.setPatronymic(txtPatronymic.getText());
+        clientDao.update(client);
+        factory.close();
 
+    }
+
+    private void getGender(){
+        SessionFactory factory=new Configuration().configure().buildSessionFactory();
+        GenderService genderService=new GenderService(factory);
+        genders.addAll(genderService.readByAll());
     }
 }
